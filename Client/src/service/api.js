5a0af85a9bcbf737aff590d3a100254fc3c28e68
Lokesh Ghosh <lghosh857@gmail.com>
2024-02-15 01:29:57 +0530

    client update : set up dotenv

--- Client/src/service/api.js
+++ Client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const todosApi = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/todos'
+    baseURL: import.meta.env.VITE_BASE_URL
 })
 
 class ApiService {
